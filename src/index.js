031581387f2b317f1ebcd7f328cdee4272190107
Murc96 <marc.schori96mail.com>
2023-10-05 14:40:07 +0200

    changed tiles to objects and gave them datasets to fit relevant info

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import { Gameboard } from "./modules/gameboard";
 import { Player } from "./modules/player";
 import "./styles.css";
 
-let player = new Player("Marc", "Beginner");
+let player = new Player("Marc", "beginner");
 let gameboard = new Gameboard(player);
 
 gameboard.createGameboard();
 gameboard.placeMines();
+gameboard.calculateAdjacentMines();
+
+//gameboard.uncoverCell(3, 5);
 
 console.log(gameboard);
--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -6,35 +6,70 @@ class Gameboard {
     this.difficulty = player.difficulty;
   }
 
-  checkDifficulty() { }
-
   createGameboard() {
-    let z = 8;
+    let rows;
+    let cols;
 
-    for (let i = 0; i < z; i++) {
+    if (this.difficulty === "beginner") {
+      rows = 8;
+      cols = 8;
+    } else if (this.difficulty === "intermediate") {
+      rows = 16;
+      cols = 16;
+    } else if (this.difficulty === "advanced") {
+      rows = 16;
+      cols = 30;
+    }
+
+    for (let x = 0; x < rows; x++) {
       let innerArray = [];
-      this.board.push(innerArray);
-      for (let j = 0; j < z; j++) {
-        innerArray.push(0);
+      for (let y = 0; y < cols; y++) {
+        const tile = {
+          x,
+          y,
+          mine: false,
+          hidden: true,
+          adjacentMines: 0,
+        }
+        innerArray.push(tile);
       }
+      this.board.push(innerArray);
     }
   }
 
   placeMines() {
-    let z = 10;
-    for (let i = 0; i < z; i++) {
-      let x = parseInt(Math.random() * 8);
-      let y = parseInt(Math.random() * 8);
+    let z;
+    let rows;
+    let cols;
 
-      if (this.board[x][y] == 0) {
-        this.board[x][y] = "x";
-      } else if (this.board[x][y] == "x") {
-        // Look for a spot that doesn't have an "x"
+    if (this.difficulty === "beginner") {
+      z = 10;
+      rows = 8;
+      cols = 8;
+    } else if (this.difficulty === "intermediate") {
+      z = 40;
+      rows = 16;
+      cols = 16;
+    } else if (this.difficulty === "advanced") {
+      z = 99;
+      rows = 16;
+      cols = 30;
+    }
+
+
+    for (let i = 0; i < z; i++) {
+      let x = parseInt(Math.random() * rows);
+      let y = parseInt(Math.random() * cols);
+
+      if (this.board[x][y].mine === false) {
+        this.board[x][y].mine = true;
+      } else if (this.board[x][y].mine === true) {
+        // Look for a spot that doesn't have a mine
         let found = false;
-        for (let row = 0; row < 8; row++) {
-          for (let col = 0; col < 8; col++) {
-            if (this.board[row][col] !== "x") {
-              this.board[row][col] = "x";
+        for (let row = 0; row < rows; row++) {
+          for (let col = 0; col < cols; col++) {
+            if (this.board[row][col].mine !== true) {
+              this.board[row][col].mine = true;
               found = true;
               break;
             }
@@ -46,6 +81,37 @@ class Gameboard {
       }
     }
   }
+  
+  countAdjacentMines(row, col) {
+    let count = 0;
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = col - 1; j <= col + 1; j++) {
+        // Überprüfe, ob die Position im Spielfeld liegt
+        if (i >= 0 && i < this.board.length && j >= 0 && j < this.board[i].length) {
+          // Überprüfe, ob das Feld eine Mine ist
+          if (this.board[i][j].mine === true && !(i === row && j === col)) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
+  }
+
+  calculateAdjacentMines() {
+    for (let row = 0; row < this.board.length; row++) {
+      for (let col = 0; col < this.board[row].length; col++) {
+        // Überprüfe, ob das Feld eine Mine ist; wenn ja, überspringe es
+        if (this.board[row][col].adjacentMines !== true) {
+          // Berechne die Anzahl der benachbarten Minen für das aktuelle Feld
+          let adjacentMines = this.countAdjacentMines(row, col);
+          // Weise die berechnete Anzahl der benachbarten Minen dem Feld zu
+          this.board[row][col].adjacentMines = adjacentMines;
+        }
+      }
+    }
+  }
+  
 }
 
 export { Gameboard };
